bc55b4a19279bc35fa32e440ab28f3851d0c063d
颜崔 <237387718@qq.com>
2016-12-13 15:54:53 +0800

    修改框架(二)

---
+++ AnimationDemo/app/src/main/java/com/ares/animationdemo/drawable_animation/DrawableAnimationActivity.java
@@ -0,0 +1,15 @@
+package com.ares.animationdemo.drawable_animation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ares.animationdemo.R;
+
+public class DrawableAnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawable_animation);
+    }
+}
---
+++ AnimationDemo/app/src/main/java/com/ares/animationdemo/property_animation/PropertyAnimationActivity.java
@@ -0,0 +1,15 @@
+package com.ares.animationdemo.property_animation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ares.animationdemo.R;
+
+public class PropertyAnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_property_animation);
+    }
+}
