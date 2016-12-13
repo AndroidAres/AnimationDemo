4007f3d71005da8a1a9b10544e49bc62c843eb2d
颜崔 <237387718@qq.com>
2016-12-14 00:27:10 +0800

    远程同步测试二

--- AnimationDemo/app/src/main/java/com/ares/animationdemo/drawable_animation/DrawableAnimationActivity.java
+++ AnimationDemo/app/src/main/java/com/ares/animationdemo/drawable_animation/DrawableAnimationActivity.java
@@ -1,15 +1,17 @@
 package com.ares.animationdemo.drawable_animation;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ares.animationdemo.R;
+import com.ares.animationdemo.databinding.ActivityDrawableAnimationBinding;
 
 public class DrawableAnimationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawable_animation);
+        ActivityDrawableAnimationBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_drawable_animation);
     }
 }
