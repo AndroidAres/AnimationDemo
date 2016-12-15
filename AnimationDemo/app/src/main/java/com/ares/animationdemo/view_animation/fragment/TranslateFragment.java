4c5f9db187430241e37677a218d24a0539f42231
颜崔 <237387718@qq.com>
2016-12-16 01:01:45 +0800

    Tween动画Fragment布局完成

---
+++ AnimationDemo/app/src/main/java/com/ares/animationdemo/view_animation/fragment/TranslateFragment.java
@@ -0,0 +1,10 @@
+package com.ares.animationdemo.view_animation.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2016/12/16.
+ */
+
+public class TranslateFragment extends Fragment {
+}
